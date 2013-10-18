8450283cb5a02c07a44c7a93caa4ecff28410cf8
wesgeorge <wesley.e.george@gmail.com>
2013-10-18 14:10:24 -0400

    Update checker.pl
    changes to logic and error handling

--- checker.pl
+++ checker.pl
@@ -12,94 +12,98 @@ close(URLS);
 my @v6domains=();
 my $outputfile="result.csv";
 open(OUTPUT, '>',$outputfile);
+print OUTPUT "site,error,csslinks,badcss,imges,badimgs,linkds,badlinks\n";
 
-        print OUTPUT "site,error,csslinks,badcss,imges,badimgs,linkds,badlinks\n";
 foreach (@domains) {
     chomp;
     my $website = "$_";
     $website =~ s/\s*$//;
+    my $v6IP = `dig aaaa +short $website`;
+    if (!$v6IP) {
+       print OUTPUT "$website,no aaaa record.\n";
+       print "$website no aaaa record.\n";
+       next;
+    }
     my $htmllogfile = $website.".log";
     my $webfile = $website.".html";
     `wget -6 -k -T 3 -t 3 --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" -O "$webfile" -P /dev/null -o "$htmllogfile" "$website" 2>/dev/null`;
+    my @ST = stat($webfile);
+    my $htmlfilelength = $ST[7];
 
-    open(HTMLLOGFILE, $htmllogfile) or die "Log file was not created\n";
-    my @lines = <HTMLLOGFILE>;
-    close HTMLLOGFILE;
+    open my $fh, '<', $htmllogfile or die "Log file was not created\n";
+    my $wgetlog = do { local $/; <$fh> };
+    close $fh;
     unlink($htmllogfile);
 
-    foreach (@lines){
-        chomp;
-        #ssl failures
-        if ("$_" eq "Unable to establish SSL connection."
-            or substr("$_", 0, 16) eq "Resource Locator"){
+    #ssl failures
+    if ("$wgetlog" =~ /.*Unable to establish SSL connection\..*/si ||
+        "$wgetlog" =~ /Resource Locator/si) {
             print OUTPUT "$website,SSL fault.\n";
             print  "$website SSL fault.\n";
+            unlink($htmllogfile);
             unlink($webfile);
-            last;
-        }
-        #Successful website load
-        elsif ("$_" eq "HTTP request sent, awaiting response... 200 OK"){
-            push(@v6domains, $website);
-            print "$website reachable via IPv6, scheduling additional tests.\n";
-            last;
-        }
-        #Login information required
-        elsif("$_" eq "Authorization failed."){
+    #Successful website load
+    } elsif ("$wgetlog" =~ /.*HTTP request sent, awaiting response\.\.\.\s+200\s+OK/si) {
+            print "$website reachable via IPv6, running additional tests.\n";
+            unlink($htmllogfile);
+            runchecks($website);
+    #Login information required
+    } elsif("$wgetlog" =~ /Authorization failed\./si) {
             print OUTPUT "$website,Authorization required.\n";
             print  "$website, Authorization required.\n";
+            unlink($htmllogfile);
             unlink($webfile);
-            last;
-        }
-        #Internal server error - ERROR 500
-        elsif(substr("$_,", 0, 43) eq "HTTP request sent, awaiting response... 500"){
+    #Internal server error - ERROR 500
+    } elsif("$wgetlog" =~ /HTTP request sent, awaiting response\.\.\.\s+500.*/si) {
             print OUTPUT "$website,Internal Server Error.\n";
             print "$website, Internal Server Error.\n";
+            unlink($htmllogfile);
             unlink($webfile);
-            last;
-        }
-        #Forbidden site - ERROR 403
-        elsif(substr("$_,", 0, 43) eq "HTTP request sent, awaiting response... 403"){
+    #Forbidden site - ERROR 403
+    } elsif("$wgetlog" =~ /HTTP request sent, awaiting response\.\.\.\s+403.*/si) {
             print OUTPUT "$website,Forbidden Error.\n";
             print "$website, Forbidden Error.\n";
+            unlink($htmllogfile);
             unlink($webfile);
-            last;
-        }
-        #Website not found - ERROR 404
-        elsif(substr("$_,", 0, 43) eq "HTTP request sent, awaiting response... 404"){
+    #Website not found - ERROR 404
+    } elsif("$wgetlog" =~ /HTTP request sent, awaiting response\.\.\.\s+404.*/si) {
             print OUTPUT "$website,Website Not Found Error.\n";
             print "$website, Website Not Found Error.\n";
             unlink($webfile);
-            last;
-        }
-        #Service unavailable - ERROR 503
-        elsif(substr("$_,", 0, 43) eq "HTTP request sent, awaiting response... 503"){
+            unlink($htmllogfile);
+    #Service unavailable - ERROR 503
+    } elsif("$wgetlog" =~ /HTTP request sent, awaiting response\.\.\.\s+503.*/si) {
             print OUTPUT "$website,Service Unavailable Error.\n";
             print "$website, Service Unavailable Error.\n";
             unlink($webfile);
-            last;
-        }
-        #Connection time out or refused
-        elsif(substr("$_", -19) eq "Connection refused."
-              or "$_" eq "Giving up."
-              or "$_" eq "20 redirections exceeded."){
+            unlink($htmllogfile);
+    #Connection time out or refused
+    } elsif("$wgetlog" =~ /.*Connection refused.*/si ||
+            "$wgetlog" =~ /.*Giving up\..*/si ||
+            "$wgetlog" =~ /.*20 redirections exceeded\..*/ ) {
             print OUTPUT "$website,Connection Refused or Timeout.\n";
             print "$website, Connection Refused or Timeout.\n";
+            unlink($htmllogfile);
             unlink($webfile);
-            last;
-        }
-        #Not IPv6 compatible
-        elsif ( substr("$_", 0, 5) eq "wget:"){
+    #Not IPv6 compatible
+    } elsif("$wgetlog" =~ /wget:.*/si){
             print OUTPUT "$website,No IPv6 server.\n";
             print  "$website, No IPv6 server.\n";
             unlink($webfile);
-            last;
-        }
+            unlink($htmllogfile);
+    } elsif ("$htmlfilelength" == 0) {
+            print OUTPUT "$website,Zero length HTML returned.\n";
+            print  "$website Zero length htmlfile.\n";
+            unlink($webfile);
+            unlink($htmllogfile);
+    } else {
+            print OUTPUT "$website,Unknown Issue - no status determined check $webfile and $htmllogfile\n";
     }
 }
+close(OUTPUT);
 
-foreach (@v6domains){
-        chomp;
-        my $website="$_";
+sub runchecks {
+        my $website="$_[0]";
         my $webfile="$website.html";
         my $parser = HTML::TokeParser::Simple->new($webfile);
         #########################################################################
@@ -144,7 +148,7 @@ foreach (@v6domains){
         print "\nChecking $website CSS";
         while( my $css = $parser->get_tag('link') ){
             my $rel = $css->get_attr('rel');
-            if ($rel =~ /stylesheet/i ) {
+            if ($rel && $rel =~ /stylesheet/i ) {
                 my $href = $css->get_attr('href');
                 if(defined $href){
                      my $logFile = "$website.csslog.txt";
@@ -174,37 +178,41 @@ foreach (@v6domains){
         ### check local links
         #########################################################################
         $parser = HTML::TokeParser::Simple->new($webfile);
+        my $domain = $website;
+        $domain =~ s/.*?([^.]+\.[^.]+)$/$1/;
         my $acnt = 1;
         my $abadcnt = 0;
         print "\nChecking $website local links";
         while( my $a = $parser->get_tag('a') ){
             my $href = $a->get_attr('href');
 
+
             #testing if link is within the domain
-            if (defined $href
-                and $href =~ /http(s|):\/\/$website.*/
-                and $acnt < 15){
+            if (defined $href) {
+                if ( $href =~ /http(s|):\/\/.*$domain.*/
+                   and $acnt < 15){
 
-                my $logFile = "$website.alog.txt";
-                print ".";
-                `wget --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" -6 -T 4 -t 4 -O /dev/null -o $logFile --no-check-certificate "$href" 2>/dev/null`;
+                   my $logFile = "$website.alog.txt";
+                   print ".";
+                   `wget --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3" -6 -T 4 -t 4 -O /dev/null -o $logFile --no-check-certificate "$href" 2>/dev/null`;
 
-                open(LOGFILE, $logFile) or die "Log file was not created\n";
-                my @lines = <LOGFILE>;
-                close(LOGFILE);
-                unlink($logFile);
+                   open(LOGFILE, $logFile) or die "Log file was not created\n";
+                   my @lines = <LOGFILE>;
+                   close(LOGFILE);
+                   unlink($logFile);
 
-                foreach (@lines){
-                    chomp;
-                    if (substr("$_", 0, "5") eq "wget:"
-                        or substr("$_", -19) eq "Connection refused."
-                        or "$_" eq "Giving up."
-                        or "$_" eq "20 redirections exceeded."){
-                        $abadcnt++;
-                    }
+                   foreach (@lines){
+                       chomp;
+                       if (substr("$_", 0, "5") eq "wget:"
+                           or substr("$_", -19) eq "Connection refused."
+                           or "$_" eq "Giving up."
+                           or "$_" eq "20 redirections exceeded."){
+                           $abadcnt++;
+                       }
+                   }
+                   ###print "$website has good v6 link: $href count: $acnt bads: $abadcnt\n";
+                   $acnt++;
                 }
-                ###print "$website has good v6 link: $href count: $acnt bads: $abadcnt\n";
-                $acnt++;
             }
         }
 
@@ -212,4 +220,3 @@ foreach (@v6domains){
         print OUTPUT "$website,,$csscnt,$badcsscnt,$imgcnt,$badimgcnt,$acnt,$abadcnt\n";
         print "\n$website: CSS:$csscnt CSS failed:$badcsscnt IMG:$imgcnt IMG failed:$badimgcnt Links:$acnt Links failed:$abadcnt\n";
 }
-close(OUTPUT);
